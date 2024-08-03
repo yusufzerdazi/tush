7dba6b0bc66675b44966e12849d9d43b9d8b7b2b
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-08-03 01:21:41 +0100

    Remove section

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,6 @@ function App() {
           We are a new music event space (founded in London) seeking to create a fun, inclusive and supportive artistic environment for all our friends (old and new!) from all backgrounds & cultures and of all genders & sexualities to dance, vibe and thrive ✨ We want everyone to feel welcome, including all those who identify as "They", "She" or "He", to bring people together as "Us".
           </p>
         </section>
-        <section className='max-w-screen-md text-center pb-10'>
-          <h2 className='text-3xl '><b>Our Principles</b></h2>
-          <p className='text-lg'><b>BEHAVE</b> - Actively promote inclusive behaviours at our events, with zero tolerance to discrimination, making sure we lead by example. (Including basic rave etiquette!)</p>
-          <p className='text-lg'><b>PROMOTE</b> - Seek to consistently involve a diverse team of artists / DJs at our events. Make space for artists to promote their own music where possible.</p>
-          <p className='text-lg'><b>SUPPORT</b> - Pledge a % of profits or ask for donations on the evening to causes that support the community: e.g. charities for LGBTQ+ rights, POC, mental health, suicide prevention, women’s rights, environmental causes, etc.</p> 
-        </section>
         <section>
           <a className="px-1 hover:opacity-70" href="https://instagram.com/tush_space" title="LinkedIn">
             <i className="fab fa-instagram fa-3x"/>
