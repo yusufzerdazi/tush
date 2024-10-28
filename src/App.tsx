b29a0f957e36e347bd14967d16f34fdd3d8c2f80
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-10-28 20:07:06 +0000

    Re-add RA

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
         <div className='pb-10'>
           <img className="max-w-80 w-full" src="tush.svg"></img>
         </div>
+        <section className='max-w-screen-md w-full text-center pb-10'>
+          <h2 className='text-3xl pb-2'><b>Upcoming Events</b></h2>
+          <iframe className='w-full' src="https://ra.co/widget/eventlisting?promoter=147421&bgcolor=242424&linkcolor=6b96c2&textcolor=dae7e0&hideimage=true" height="400" />
+        </section>
         <section className='max-w-screen-md text-center pb-10'>
           <h2 className='text-3xl '><b>What is TUSH?</b></h2>
           <p className='text-lg'>
