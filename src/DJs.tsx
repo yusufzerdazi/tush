1a33394c3e20146680fb20ca35b24b290634fee2
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-10-30 20:43:42 +0000

    djs coming soon

--- src/DJs.tsx
+++ src/DJs.tsx
@@ -2,8 +2,10 @@ function DJs() {
   return (
     <>
       <section className='max-w-screen-lg w-full text-center pb-10'>
-        <h1 className='text-5xl pb-2'><b>Events</b></h1>
-        <iframe className='w-full' src="https://ra.co/widget/eventlisting?promoter=147421&bgcolor=242424&linkcolor=6b96c2&textcolor=dae7e0" height="400" />
+        <h1 className='text-5xl pb-2'><b>DJs</b></h1>
+        <p className='text-lg'>
+            Coming soon.
+        </p>
       </section>
     </>
   )
