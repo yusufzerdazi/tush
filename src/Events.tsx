b5e64de6bc8ef93dd0ad801e5d811d3908af5603
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-10-30 20:57:11 +0000

    updates

--- src/Events.tsx
+++ src/Events.tsx
@@ -3,7 +3,19 @@ function Events() {
     <>
       <section className='max-w-screen-lg w-full text-center pb-10'>
         <h1 className='text-5xl pb-2'><b>Events</b></h1>
-        <iframe className='w-full' src="https://ra.co/widget/eventlisting?promoter=147421&bgcolor=242424&linkcolor=6b96c2&textcolor=dae7e0" height="400" />
+        <a className="link" href="https://ra.co/events/1976590">
+          <h2 className='text-3xl pb-2'>01/11/2024: Poison Peach</h2>
+          <img className="max-w-sm m-auto pb-3" src="poisonpeach.png"></img>
+        </a>
+        <p className='text-lg'>
+        Our Halloween launch party will include House & Techno DJs, immersive lighting and a unique art exhibition in the upstairs chill area of the Jaguar Shoes venue.
+        </p>
+        <p className='text-lg'>
+        All TUSH ticket sales profits are being donated to CALM - a mental health charity. Come along to have fun, be yourself and do good for mental health. This is not one to miss!
+        </p>
+        <p className='text-lg'>
+        If you can't make the event, you can still donate to the cause <a className="link" href="https://www.justgiving.com/page/tush-for-calm"><b>here</b></a>.
+        </p>
       </section>
     </>
   )
