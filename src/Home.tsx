882ed8681b1d20543e0d887d41f64042464271fb
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-11-01 11:31:30 +0000

    include image

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,11 +9,15 @@ function Home() {
                     We are a new music event space (founded in London) seeking to create a fun, inclusive and supportive artistic environment for all our friends from all backgrounds & cultures and of all genders & sexualities to dance, vibe and thrive ✨ We want everyone to feel welcome, including all those who identify as "They", "She" or "He", to bring people together as "Us".
                 </p>
             </section>
-            <section className='max-w-screen-md w-full text-center pb-10'>
+            <section className='max-w-screen-lg w-full text-center flex flex-wrap justify-center pb-5'>
                 <Link to="events"><span className='text-3xl p-2 link'><b>Events</b></span></Link>
                 <Link to="djs"><span className='text-3xl p-2 link'><b>DJs</b></span></Link>
                 <Link to="art"><span className='text-3xl p-2 link'><b>Art</b></span></Link>
-                <a href="https://instagram.com/tush_space"><span className='text-3xl pb-2 link'><b>Instagram</b></span></a>
+            </section>
+            <section>
+                <a className="pb-2 link" href="https://instagram.com/tush_space" title="LinkedIn">
+                    <i className="fab fa-instagram fa-3x"/>
+                </a>
             </section>
         </>
     )
