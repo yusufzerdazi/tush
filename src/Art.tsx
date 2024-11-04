e0903c2fde44de6fbae61762da16ff040feb0326
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-11-04 22:01:37 +0000

    adding text to art

--- src/Art.tsx
+++ src/Art.tsx
@@ -6,18 +6,50 @@ function Art() {
 
         <p className="pb-5 text-lg">Our first exhibition showcasing the below four artworks, in collab with <a className="link" href="https://linktr.ee/spiltmilklondon"><b>Spilt Milk</b></a>, is on 01/11/2024. Prints available to purchase upon request. Email us <a className="link" href="mailto:contact@tushspace.com">here</a> :)</p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-left">
           <div>
             <img src="/mobyslick.jpg"></img>
+            <p className="text-lg"><b>Siblings</b> </p>
+            <p>by <b>Moby Slick (they/them) <a className="link" href="https://instagram.com/mobyslickshop">@mobyslickshop</a></b></p>
+            <p>
+              "Siblings" depicts the artist and their two older brothers. Throughout their life, they have been a crutch during each other's lowest points, and their support has never wavered.
+            </p>
+            <p>
+              The piece llustrates how love endures through turmoil, conveying energy and connection, and highlighting the importance of family and the resilience found in those relationships.
+            </p>
           </div>
           <div>
             <img  src="/robinmooneye.jpg"></img>
+            <p className="text-lg"><b>Love on the Dancefloor</b> </p>
+            <p>by <b>Robin Mooneye (she/her) <a className="link" href="https://instagram.com/robin_mooneye">@robin_mooneye</a></b></p>
+            <p>
+            "Love on the Dancefloor" captures the magic of sharing a dance with another person, whether they are a partner, a lover, or someone just met. In that moment, the artist explores the deep connection that can form as two bodies move in harmony to the same rhythm.
+            </p>
+            <p>
+            The dancefloor becomes a sacred space where souls intertwine, allowing vulnerability and joy to flourish through movement and shared energy.
+            </p>
           </div>
           <div>
             <img src="/samuelmccrackenartworks.jpg"></img>
+            <p className="text-lg"><b>Trans Cowboys</b></p>
+            <p>by <b>Samuel McCracken (he/him) <a className="link" href="https://instagram.com/samuelmccrackenartworks">@samuelmccrackenartworks</a></b></p>
+            <p>
+            "My gender is a slutty Cowboy grandpa. I will not be elaborating further."
+            </p>
+            <p>
+              Inspired by this quote by Queer.and.Feral, the artist embraces the playful spirit of a "slutty Cowboy grandpa" as a trans masculine identity. To add a touch of camp, the gun has been swapped for a banana- "because we all know slutty grandpas might also like a banana or two".
+            </p>
           </div>
           <div>
             <img  src="/kkayarts.jpg"></img>
+            <p className="text-lg"><b>A Couple of Witches</b> </p>
+            <p>by <b>Kayleigh Hatch (she/her) <a className="link" href="https://instagram.com/kkayarts">@kkayarts</a></b></p>
+            <p>
+              Hatch's work explores the multifaceted experiences of womanhood, especially through bonding over shared traumas, joy, same-sex desire, community, and friendship.
+            </p>
+            <p>
+            Her screenprints focus on feminine queer desire and self-acceptance, symbolizing liberation through authenticity. By distorting historical, mythological, and religious narratives to center queer relationships, she aims to make historical canons more accessible to queer people.
+            </p>
           </div>
         </div>
       </section>
@@ -25,3 +57,25 @@ function Art() {
   )
 }
 export default Art
+
+//
+// Samuel McCracken (he/him)
+// Trans Cowboys
+// "My gender is a slutty Cowboy grandpa. I will not be elaborating further."
+// Inspired by this quote by Queer.and. Feral, the artist embraces the playful spirit of a "slutty Cowboy grandpa" as a trans masculine identity. To add a touch of camp, the gun has been swapped for a banana- "because we all know slutty grandpas might also like a banana or two".
+// Kayleigh Hatch (she/her)
+// A Couple of Witches
+// Hatch's work explores the multifaceted experiences of womanhood, especially through bonding over shared traumas, joy, same-sex desire, community, and friendship.
+// Her screenprints focus on feminine queer desire and self-acceptance, symbolizing liberation through authenticity. By distorting historical, mythological, and religious narratives to center queer relationships, she aims to make historical canons more accessible to queer people.
+// @samuelmccrackenartworks
+// @kkayarts
+// Robin Mooneye (she/her)
+// Love on the Dancefloor
+// "Love is on the dancefloor" captures the magic of sharing a dance with another person, whether they are a partner, a lover, or someone just met. In that moment, the artist explores the deep connection that can form as two bodies move in harmony to the same rhythm.
+// The dancefloor becomes a sacred space where souls intertwine, allowing vulnerability and joy to flourish through movement and shared energy.
+// @robin_mooneye
+// Moby Slick (they/them)
+// Siblings
+// "Siblings" depicts the artist and their two older brothers. Throughout their life, they have been a crutch during each other's their lowest points, and their support has never wavered.
+// The piece llustrates how love endures through turmoil, conveying energy and connection, and highlighting the importance of family and the resilience found in those relationships.
+// @mobyslickshop
