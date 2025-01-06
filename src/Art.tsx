e5c536e982a856900a78374397da838a884fd72f
Yusuf Zerdazi <yusuf@zerdazi.com>
2025-01-06 22:28:30 +0000

    updates

--- src/Art.tsx
+++ src/Art.tsx
@@ -4,7 +4,7 @@ function Art() {
       <section className='max-w-screen-lg w-full text-center pb-10'>
         <h1 className='text-5xl pb-2'><b>Art</b></h1>
 
-        <p className="pb-5 text-lg">Our first exhibition showcasing the below four artworks, in collab with <a className="link" href="https://linktr.ee/spiltmilklondon"><b>Spilt Milk</b></a>, is on 01/11/2024. Prints available to purchase upon request. Email us <a className="link" href="mailto:contact@tushspace.com">here</a> :)</p>
+        <p className="pb-5 text-lg">Our first exhibition showcasing the below four artworks, in collab with <a className="link" href="https://linktr.ee/spiltmilklondon"><b>Spilt Milk</b></a>, was on 01/11/2024. Prints available to purchase upon request. Email us <a className="link" href="mailto:contact@tushspace.com">here</a> :)</p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-left">
           <div>
